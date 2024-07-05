7ba6e9cad453227af97ab548b58126b56c3cc175
Rei Watanabe <164761676+Rei9005@users.noreply.github.com>
2024-07-05 14:17:53 +0900

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  resources :reviews
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'lists#index'
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
 
-  resources :bookmarks, only: [:destroy]
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
